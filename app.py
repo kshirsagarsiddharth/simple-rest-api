b13ff25217a3c2b202a2842a1375317d4653ee8a
Siddharth Kshirsagar <kshirsagarsiddharth45@gmail.com>
2022-03-23 15:43:42 +0530

    created run file

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
+
+
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/item')
 api.add_resource(UserRegister,'/register')
 api.add_resource(StoreList,'/store')
 
-from db import db 
-db.init_app(app)
 
 if __name__ == "__main__":
+    from db import db 
+    db.init_app(app)
     app.run(debug=True)
